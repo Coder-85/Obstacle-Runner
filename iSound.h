10c5393939963c7a870d9d9071a58a086a32d4ba
Coder-85 <fiskwilson34@gmail.com>
2025-06-16 04:03:51 +0600

    fix: OS independent SDL header include

--- iSound.h
+++ iSound.h
@@ -5,8 +5,13 @@
 //  Version: 1.0.0
 //
 
+#ifdef _WIN32
 #include <SDL.h>
 #include <SDL_mixer.h>
+#else
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_mixer.h>
+#endif
 #include <stdio.h>
 using namespace std;
 
