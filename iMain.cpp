f36d13b63e50c707ab6317ea5499d1d94ea68ca2
sifat5532 <sifat.a.5532@gmail.com>
2025-06-16 01:36:36 +0600

    feat: home page options

--- iMain.cpp
+++ iMain.cpp
@@ -1,5 +1,23 @@
 #include "iGraphics.h"
 
+#define SCRN_WIDTH 1000
+#define SCRN_HEIGHT 600
+
+enum page_status{
+    HOME,
+    PLAY,
+    RESUME,
+    LEADERBOARD,
+    SCENES,
+    HELP
+};
+enum page_status currentPage; // To track which page is being visited currently
+
+
+Image home_coin_img; // image of coin on the home page
+
+
+int home_option_color[6] = {0};
 /*
 function iDraw() is called again and again by the system.
 */
@@ -7,7 +25,82 @@ void iDraw()
 {
     // place your drawing codes here
     iClear();
-    iText(140, 180, "Biday Prithibi");
+    if(currentPage == HOME){
+        char total_coins[10] = "1000";
+        iClear();
+        // Load Bg Img
+        iShowImage(0, 0, "assets/img/bg/home_bg_002.jpg");
+        // Load Coin Img
+        iLoadImage(&home_coin_img, "assets/img/objects/coin/coin_small.png");
+        iScaleImage(&home_coin_img, 0.5);
+        iShowLoadedImage(820, 570, &home_coin_img);
+        // Print Total Coin
+        iTextAdvanced(850, 575, total_coins, 0.1, 1, GLUT_STROKE_MONO_ROMAN);
+
+        // Add options
+
+        if(home_option_color[0] == 0){
+            iSetColor(255, 255, 255);
+            iRectangle(690, 450, 250, 40);
+        }else{
+            iSetTransparentColor(2, 168, 77, 0.4);
+            iFilledRectangle(690, 450, 250, 40);
+        }
+        iSetColor(255, 255, 255);
+        iTextAdvanced(755, 463, "New Game", 0.15, 1, GLUT_STROKE_MONO_ROMAN);
+
+        if(home_option_color[1] == 0){
+            iSetColor(255, 255, 255);
+            iRectangle(690, 380, 250, 40);
+        }else{
+            iSetTransparentColor(2, 168, 77, 0.4);
+            iFilledRectangle(690, 380, 250, 40);
+        }
+        iSetColor(255, 255, 255);
+        iTextAdvanced(740, 393, "Saved Game", 0.15, 1, GLUT_STROKE_MONO_ROMAN);
+
+        if(home_option_color[2] == 0){
+            iSetColor(255, 255, 255);
+            iRectangle(690, 310, 250, 40);
+        }else{
+            iSetTransparentColor(2, 168, 77, 0.4);
+            iFilledRectangle(690, 310, 250, 40);
+        }
+        iSetColor(255, 255, 255);
+        iTextAdvanced(770, 323, "Scenes", 0.15, 1, GLUT_STROKE_MONO_ROMAN);
+
+        if(home_option_color[3] == 0){
+            iSetColor(255, 255, 255);
+            iRectangle(690, 240, 250, 40);
+        }else{
+            iSetTransparentColor(2, 168, 77, 0.4);
+            iFilledRectangle(690, 240, 250, 40);
+        }
+        iSetColor(255, 255, 255);
+        iTextAdvanced(730, 253, "Leaderboard", 0.15, 1, GLUT_STROKE_MONO_ROMAN);
+
+        if(home_option_color[4] == 0){
+            iSetColor(255, 255, 255);
+            iRectangle(690, 170, 250, 40);
+        }else{
+            iSetTransparentColor(2, 168, 77, 0.4);
+            iFilledRectangle(690, 170, 250, 40);
+        }
+        iSetColor(255, 255, 255);
+        iTextAdvanced(785, 183, "Help", 0.15, 1, GLUT_STROKE_MONO_ROMAN);
+
+        if(home_option_color[5] == 0){
+            iSetColor(255, 255, 255);
+            iRectangle(690, 100, 250, 40);
+        }else{
+            iSetTransparentColor(168, 2, 2, 0.4);
+            iFilledRectangle(690, 100, 250, 40);
+        }
+        iSetColor(255, 255, 255);
+        iTextAdvanced(785, 113, "Exit", 0.15, 1, GLUT_STROKE_MONO_ROMAN);
+
+
+    }
 }
 
 /*
@@ -16,7 +109,45 @@ function iMouseMove() is called when the user moves the mouse.
 */
 void iMouseMove(int mx, int my)
 {
-    // place your codes here
+    // when mouse hovering the home page options
+    if(currentPage == HOME){
+
+        if((mx>=690 && mx<=940) && (my>=450 && my<=490)){
+            home_option_color[0] = 1;
+        }else{
+            home_option_color[0] = 0;
+        }
+
+        if((mx>=690 && mx<=940) && (my>=380 && my<=420)){
+            home_option_color[1] = 1;
+        }else{
+            home_option_color[1] = 0;
+        }
+
+        if((mx>=690 && mx<=940) && (my>=310 && my<=350)){
+            home_option_color[2] = 1;
+        }else{
+            home_option_color[2] = 0;
+        }
+
+        if((mx>=690 && mx<=940) && (my>=240 && my<=280)){
+            home_option_color[3] = 1;
+        }else{
+            home_option_color[3] = 0;
+        }
+
+        if((mx>=690 && mx<=940) && (my>=170 && my<=210)){
+            home_option_color[4] = 1;
+        }else{
+            home_option_color[4] = 0;
+        }
+
+        if((mx>=690 && mx<=940) && (my>=100 && my<=140)){
+            home_option_color[5] = 1;
+        }else{
+            home_option_color[5] = 0;
+        }
+    }
 }
 
 /*
@@ -36,7 +167,7 @@ void iMouse(int button, int state, int mx, int my)
 {
     if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
     {
-        // place your codes here
+        printf("%d %d\n", mx, my);
     }
     if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN)
     {
@@ -96,6 +227,6 @@ int main(int argc, char *argv[])
 {
     glutInit(&argc, argv);
     // place your own initialization codes here.
-    iInitialize(400, 400, "demooo");
+    iInitialize(SCRN_WIDTH, SCRN_HEIGHT, "Obstacle Runner");
     return 0;
 }
